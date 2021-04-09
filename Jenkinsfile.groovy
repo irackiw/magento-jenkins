b38db2de14bd24a7f3cd842cc75a3614da535de0
Wojciech Iracki <irackiw@gmail.com>
2021-04-09 19:28:49 +0200

    jenkinsfile

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,55 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Pull new version') {
+            steps {
+                sh("git pull")
+            }
+        }
+        stage('Fix permissions') {
+            steps {
+                sh("chmod u+x bin/magento")
+            }
+        }
+        stage('Composer install') {
+            steps {
+                sh("composer install")
+                sh("rm -rf var/.regenerate")
+            }
+        }
+        stage('Setup upgrade') {
+            steps {
+                sh("bin/magento maintenance:enable")
+                sh("bin/magento setup:upgrade")
+                sh("bin/magento maintenance:disable")
+            }
+        }
+        stage('DI compile') {
+            steps {
+                sh("bin/magento setup:di:compile")
+            }
+        }
+        stage('Static content deploy') {
+            steps {
+                sh("bin/magento setup:static-content:deploy")
+            }
+        }
+        stage('Change symlinks') {
+            steps {
+                sh("ln -s ")
+            }
+        }
+        stage('Magento cache clear') {
+            steps {
+                sh("bin/magento cache:clean")
+                sh("bin/magento cache:enable")
+            }
+        }
+        stage('Cloudflare cache clear') {
+            steps {
+                echo 'Deploying....'
+            }
+        }
+    }
+}
